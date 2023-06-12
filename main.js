453f7ccd824724e024c0a152040f921980d017dc
Jason <jyshowalter@gmail.com>
2023-06-11 22:02:33 -0700

    finished lab

--- main.js
+++ main.js
@@ -3,10 +3,31 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const modal = document.querySelector("#modal")
+const likeButton = document.querySelector("li.like")
+const heart = document.querySelectorAll(".like-glyph")
 
+likeButton.addEventListener ("click", like)
 
-
-
+function like(event) {
+  let like = event.target;
+  mimicServerCall()
+  .then(() =>  {
+    if (like.textContent === EMPTY_HEART) {
+      like.className = "activated-heart";
+      like.innerHTML = FULL_HEART;
+    }
+    else {
+      like.className = "";
+      like.innerHTML = EMPTY_HEART;
+    }
+  })
+  .catch(() => {
+    modal.className = "";
+    setTimeout(() => (modal.className = "hidden"), 3000);
+    like.innerHTML = EMPTY_HEART;
+  })
+}
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
